0a0093690549f645a7ae62fad8a06e90caf50d28
Forever Young <dy9040@gmail.com>
2022-05-07 11:43:14 -0500

    conditional rendering the WPA so when you click on the nav bar the correct items show up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-import Gallery from './components/Gallery'
+import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 import './App.css';
 
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
   const [categories] = useState([
     {
       name: 'commercial',
@@ -25,10 +27,18 @@ function App() {
        categories={categories}
        setCurrentCategory={setCurrentCategory}
        currentCategory={currentCategory}
+       contactSelected={contactSelected}
+       setContactSelected={setContactSelected}
        ></Nav>
       <main>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+     {!contactSelected ? (
+       <>
+       <Gallery currentCategory={currentCategory}></Gallery>
+       <About></About>
+       </>
+     ) : (
+       <ContactForm></ContactForm>
+     )}
       </main>
     </div>
   );
