e628cda544653fa9af02993b732d2d2400171bd5
Forever Young <dy9040@gmail.com>
2022-05-08 17:35:41 -0500

    created a modal

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import Modal from '../Modal'
 
 
 const PhotoList = ({ category }) => {
+  const [isModalOpen, setIsModalOpen] =useState(false);
 
   const [photos] = useState([
     {
@@ -100,17 +102,26 @@ const PhotoList = ({ category }) => {
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
     },
   ]);
-
+  const [currentPhoto, setCurrentPhoto] = useState();
   const currentPhotos = photos.filter((photo) => photo.category === category);
+  const toggleModal =(image, i) => {
+    // current photo
+    setCurrentPhoto({...image, index: i})
+    setIsModalOpen(!isModalOpen);
+  }
 
   return (
     <div>
+     {isModalOpen && (
+  <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+)}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
             src={require(`../../assets/small/${category}/${i}.jpg`)}
             alt={image.name}
             className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
             key={image.name}
           />
         ))}
